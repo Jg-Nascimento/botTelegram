8ccdd3fe37a2c79b6a6f6de3ca1583751f24e455
jorgetstechnology <47434531+jorgetstechnology@users.noreply.github.com>
2020-04-28 17:00:09 -0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+#1194714850:AAGsCicfXKJrK4Z_QICDYxF6_G9jNYalMu8
+from telegram.ext import Updater, CommandHandler
+
+def welcome(update, content):
+    message = 'Olá usuário, ' + update.message.from_user.first_name + '!'
+    print(message)
+    content.bot.send_message(chat_id = update.effective_chat.id, text = message)
+
+def main():
+    token = '1194714850:AAGsCicfXKJrK4Z_QICDYxF6_G9jNYalMu8'
+    updater = Updater(token = token, use_context = True)
+    updater.start_polling()
+    print('Updater' + str(updater))
+    updater.dispatcher.add_handler(CommandHandler('start', welcome))
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
+
